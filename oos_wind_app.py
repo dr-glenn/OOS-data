7540f387365eed3c3d85dd915492f88354702076
Glenn Nelson <kitecamguy@gmail.com>
2020-03-22 16:31:50 -0700

    Showing all four graphs. Now work to improve style.

--- oos_wind_app.py
+++ oos_wind_app.py
@@ -22,10 +22,12 @@ df = df.drop_duplicates()
 df.index = pd.to_datetime(df.index,format="%Y%m%d%H%M")
 # the full index range
 date_limits = [df.index.min(),df.index.max()]
-# start with range slider of last 30 days
-date_range = date_limits[1] - date_limits[0]
-date1 = date_limits[1]
-date0 = date_limits[1] - dt.timedelta(days=30) if date_range > dt.timedelta(days=30) else date_limits[0]
+# start with range slider of last N days
+N_DAYS = 20
+date1 = dt.date(date_limits[1].year, date_limits[1].month, date_limits[1].day)
+date0 = dt.date(date_limits[0].year, date_limits[0].month, date_limits[0].day)
+date_range = date1 - date0
+date0 = date0 if date_range <= dt.timedelta(days=N_DAYS) else date1 - dt.timedelta(days=N_DAYS)
 
 df30 = df.loc[date0 : date1]
 
@@ -52,6 +54,17 @@ def get_day_indices(df):
 
 day_marks = get_day_indices(df)
 
+# generate a list of dates for the slider
+def gen_date_list(date0,date1):
+    d0 = dt.date(date0.year,date0.month,date0.day)
+    d1 = dt.date(date1.year,date1.month,date1.day)
+    ndate = (d1-d0).days
+    day_delta = dt.timedelta(days=1)
+    dlist = [d0 + i*day_delta for i in range(ndate)]
+    return dlist
+
+day_list = gen_date_list(date0,date1)
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 #app = dash.Dash(__name__)
 
@@ -64,35 +77,30 @@ app.layout = html.Div(children=[
     # Next Div contains two graphs side-by-side, using display="inline-block"
     html.Div(children=[
         # XY graph of wind speed, time on x-axis
-        html.Span(dcc.Graph(
+        html.Span(
+            dcc.Graph(
                 id='wind-date-plot',
-                figure={
-                    'layout': {
-                        'title': 'Winds from Oliver Observing Station'
-                    }
-                }
             ),
             style={"display":"inline-block","width":"60%"}
         ),
         # polar plot of wind speed and direction
-        html.Span([dcc.Graph(
+        html.Span([
+            dcc.Graph(
                 id='wind-dir-plot',
-                figure={
-                    'layout': {
-                        'title': 'Winds from Oliver Observing Station'
-                    }
-                }
             ),
-            dcc.Slider(
-                id='hour-slider',
-                min=0,
-                max=23,
-                value=0,
-                marks={str(hour): str(hour) for hour in range(0,23,2)},
-                step=None
+            html.Div(
+                dcc.Slider(
+                    id='hour-slider',
+                    min=0,
+                    max=23,
+                    value=0,
+                    marks={str(hour): str(hour) for hour in range(0,23,2)},
+                    step=None
+                ),
+                style={'margin-top':'20px'},
             )],
             
-            style=dict(display="inline-block",width="25%")
+            style=dict(display="inline-block",width="20%")
         ),
         ]
     ),
@@ -102,13 +110,30 @@ app.layout = html.Div(children=[
         dcc.Slider(
             id='wind-date-slider',
             min = 0,
-            max = len(day_marks)-2,    # -2 because we cannot select last date as start point
+            max = len(day_list)-2,    # -2 because we cannot select last date as start point
             value = 0,
-            #marks={i: 'Day {}'.format(i) if i == 0 else str(i) for i in range(0,len(day_marks))},
-            marks={i: {'label':str(dm[0]),'style':{'transform':'rotate(40deg)'}} for i,dm in enumerate(day_marks) if i%2 == 0},
+            #marks={i: {'label':str(dm),'style':{'transform':'rotate(40deg) translate(0px,20px)'}} for i,dm in enumerate(day_list) if i%2 == 0},
+            marks={i: {'label':str(dm),'style':{'transform':'rotate(-40deg) translate(-40px,-20px)'}} for i,dm in enumerate(day_list) if i%2 == 0},
+            dots=True,
             # transform: rotate(20deg)
         ),
-        style={"width":"80%","margin-top":"30px","margin-left":"auto","margin-right":"auto"}
+        style={"width":"40%","margin-top":"0px","margin-left":"10%","margin-bottom":"50px"}
+    ),
+    # Second row of graphs
+    html.Div(children=[
+        html.Span(
+            dcc.Graph(
+                id='temp-out',
+            ),
+            style={'display':'inline-block'},
+        ),
+        html.Span(
+            dcc.Graph(
+                id='rel-hum',
+            ),
+            style={'display':'inline-block'},
+        ),
+        ]
     ),
 ])
 
@@ -117,15 +142,39 @@ app.layout = html.Div(children=[
     Output('wind-date-plot', 'figure'),
     [Input('wind-date-slider', 'value')])
 def update_wind_speed_figure(selected_day):
-    f_df = df[day_marks[selected_day][1]:day_marks[selected_day+1][1]]
-    #f_df = df30[day_marks[selected_day].key()]
+    f_df = df30[str(date0+dt.timedelta(days=selected_day))]
     traces = []
     traces.append(dict(x=f_df.index,y=f_df['wind_speed'],type='line',name=u'wind_speed'))
     traces.append(dict(x=f_df.index,y=f_df['wind_gust'],type='line',name=u'wind_gust'))
 
     return {
         'data': traces,
-        'layout': dict(title='Wind Speed'),
+        'layout': dict(title='Wind Speed (every 5 minutes)'),
+    }
+
+@app.callback(
+    Output('temp-out', 'figure'),
+    [Input('wind-date-slider', 'value')])
+def update_temp_out_figure(selected_day):
+    f_df = df30[str(date0+dt.timedelta(days=selected_day))]
+    traces = []
+    traces.append(dict(x=f_df.index,y=f_df['temp_out'],type='line',name=u'temperature'))
+    return {
+        'data': traces,
+        'layout': dict(title='Temperature Outside', height=400),
+    }
+
+@app.callback(
+    Output('rel-hum', 'figure'),
+    [Input('wind-date-slider', 'value')])
+def update_rel_hum_figure(selected_day):
+    f_df = df30[str(date0+dt.timedelta(days=selected_day))]
+    traces = []
+    traces.append(dict(x=f_df.index,y=f_df['humidity'],type='line',name=u'humidity'))
+
+    return {
+        'data': traces,
+        'layout': dict(title='Relative Humidity', height=400),
     }
 
 
@@ -134,14 +183,18 @@ def update_wind_speed_figure(selected_day):
     [Input('wind-date-slider', 'value'),
      Input('hour-slider','value')])
 def update_wind_dir_figure(selected_day, selected_hour):
-    f_df = df[day_marks[selected_day][1]:day_marks[selected_day+1][1]]
-    date0 = f_df.index[0]   # it's a datetime
+    f_df = df30[str(date0+dt.timedelta(days=selected_day))]
+    # Max wind speed in this time
+    wmax = f_df['wind_speed'].max()
+    wmax = int(round(wmax,-1))
+    tick_int = 10 if wmax >= 20 else 5
+    # Get 4 hour slice for this graph
+    d0 = f_df.index[0]   # it's a datetime
     hour0 = dt.timedelta(hours=selected_hour)
     h1 = selected_hour+4 if selected_hour <= 20 else 24
     hour1 = dt.timedelta(hours=h1)
-    #f_df = f_df[f_df.index <= date0+hour1]
-    #f_df = f_df[f_df.index >= date0 and f_df.index <= date0+hour1]
-    f_df = f_df.loc[date0+hour0 : date0+hour1]
+    f_df = f_df.loc[str(d0+hour0) : str(d0+hour1)]
+    # Wind direction of 359 is almost certainly bogus, so remove
     winds = f_df.loc[df['wind_dir'] != 359.0][['wind_dir','wind_speed']]
     val = winds["wind_speed"]
     direction = winds["wind_dir"]
@@ -158,17 +211,17 @@ def update_wind_dir_figure(selected_day, selected_hour):
 
     layout = dict(
         height=350,
-        plot_bgcolor=app_color["graph_bg"],
+        #plot_bgcolor=app_color["graph_bg"],
         paper_bgcolor=app_color["graph_bg"],
         font={"color": "#fff"},
         autosize=False,
         polar=dict(
             bgcolor=app_color["graph_line"],
-            radialaxis=dict(range=[0, 40], angle=0, dtick=10),
+            radialaxis=dict(range=[0, wmax], angle=0, dtick=tick_int),
             angularaxis=dict(showline=False, tickcolor="white",rotation=90,direction='clockwise'),
         ),
         showlegend=False,
-        title='Wind Direction',
+        title='Wind Direction (4 hours)',
     )
 
     return {
