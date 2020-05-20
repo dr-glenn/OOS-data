b4fddaf1db2e98545b286b9fb073a1619a8a5048
dr-glenn <kitecamguy@gmail.com>
2020-05-20 14:34:32 -0700

    Improve UI layout

--- oos_wind_app.py
+++ oos_wind_app.py
@@ -6,17 +6,44 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import datetime as dt
+import logging
+import logging.handlers
+# log files are max size 50K, then rollover to 5 backups
+handler = logging.handlers.RotatingFileHandler('/home/jkhyrsmy/pyapps/OOS-data/oos_wind_app.log', maxBytes=100000, backupCount=5)
+formatter = logging.Formatter(fmt='%(asctime)s : %(message)s')    # defines the format of each logged message
+handler.setFormatter(formatter)
+# the root logger is inherited by all modules that are used in your program
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)    # alternative, use string 'INFO'
+root_logger.addHandler(handler)
+
+# logger for this module
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logger.info('start')
 
 # Dash callbacks: https://dash.plot.ly/getting-started-part-2
 
 # wind direction polar plot with dcc: https://github.com/plotly/dash-sample-apps/blob/master/apps/dash-wind-streaming/app.py
 
+"""
+TODO:
+- style so that graphs are nicer size
+- style so that graphs don't have so much border space
+- radio selector for winds displayed on polar chart: wind or gust
+- open page at most recent date and time
+- selector for number of hours to display on polar plot
+- date-time range slider so that user can contorl time span on x-axis
+"""
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
 
+"""
 here_dir = os.path.dirname(__file__)
-#datafile = '/home/jkhyrsmy/pyapps/OOS-data/data/oosdata.2020'
 datafile = os.path.join(here_dir, 'data/oosdata.2020')
+"""
+datafile = '/home/weather/OOSwdata/OOSwdata.2020'
 dat_names = ['wdate','wind_dir','wind_speed','wind_gust','col5','humidity','col7','temp_out','col9','col10']
 # TODO: can I read just a select date range?
 df = pd.read_csv(datafile, sep=' ',names=dat_names,index_col=0,parse_dates=True)
@@ -25,14 +52,22 @@ df = df.drop_duplicates()
 df.index = pd.to_datetime(df.index,format="%Y%m%d%H%M")
 # the full index range
 date_limits = [df.index.min(),df.index.max()]
+logger.debug('data index range: %s to %s' %(str(df.index.min()),str(df.index.max())))
+
 # start with range slider of last N days
 N_DAYS = 20
+# use datetime, set earliest to 00:00 and latest to 23:59
+datetime1 = dt.datetime(date_limits[1].year, date_limits[1].month, date_limits[1].day,hour=23,minute=59)
 date1 = dt.date(date_limits[1].year, date_limits[1].month, date_limits[1].day)
+datetime0 = dt.datetime(date_limits[0].year, date_limits[0].month, date_limits[0].day)
 date0 = dt.date(date_limits[0].year, date_limits[0].month, date_limits[0].day)
-date_range = date1 - date0
+date_range = date1 - date0  # integer number of days
+# date0 no more than N_DAYS before date1
 date0 = date0 if date_range <= dt.timedelta(days=N_DAYS) else date1 - dt.timedelta(days=N_DAYS)
+logger.debug('date range: %s to %s' %(str(date0),str(date1)))
 
-df30 = df.loc[date0 : date1]
+df30 = df.loc[datetime0 : datetime1]
+logger.debug('df30 min/max = %s, %s' %(str(df30.index.min()),str(df30.index.max())))
 
 # TODO: I can select specific date like this: df.loc['2020-02-06'], even if the index contains HHMMSS.
 # TODO: How to select a range of dates?
@@ -41,6 +76,7 @@ df30 = df.loc[date0 : date1]
 # TODO: make a range slider to select hours, esp. for wind_dir polar plot
 
 # get Dataframe index numbers for first record of each day
+# NOTE: not used
 def get_day_indices(df):
     '''
     :return: days: list of tuple(str(YYYY-mm-DD),loc in df.index)
@@ -55,21 +91,22 @@ def get_day_indices(df):
             days.append((dt_key,dt_keys[dt_key]))
     return days
 
-day_marks = get_day_indices(df)
-
 # generate a list of dates for the slider
 def gen_date_list(date0,date1):
     d0 = dt.date(date0.year,date0.month,date0.day)
     d1 = dt.date(date1.year,date1.month,date1.day)
-    ndate = (d1-d0).days
+    ndate = (d1-d0).days + 1
     day_delta = dt.timedelta(days=1)
     dlist = [d0 + i*day_delta for i in range(ndate)]
     return dlist
 
 day_list = gen_date_list(date0,date1)
+logger.debug('day_list: %s' %(str(day_list)))
 
 #app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash.Dash(__name__)
+
+wind_date_fig = {'layout': dict(height=400,title={'text':'Wind Speed (every 5 minutes)','y':'0.8'})}
 
 app.layout = html.Div(children=[
     html.Div([
@@ -87,18 +124,18 @@ app.layout = html.Div(children=[
         html.Span([
             dcc.Graph(
                 id='wind-date-plot',
+                #figure=wind_date_fig
             ),
             html.Div(
                 # date slider sits underneath the wind-speed plot
                 dcc.Slider(
                     id='wind-date-slider',
+                    included = False,
                     min = 0,
-                    max = len(day_list)-2,    # -2 because we cannot select last date as start point
-                    value = 0,
-                    #marks={i: {'label':str(dm),'style':{'transform':'rotate(40deg) translate(0px,20px)'}} for i,dm in enumerate(day_list) if i%2 == 0},
+                    max = len(day_list)-1,
+                    value = len(day_list)-1,    # start at last day
                     marks={i: {'label':str(dm),'style':{'transform':'rotate(-40deg) translate(-40px,-20px)'}} for i,dm in enumerate(day_list) if i%2 == 0},
                     dots=True,
-                    # transform: rotate(20deg)
                 ),
                 # style for Div that contains Slider
                 style={'width':"70%","margin-left":"100px",}
@@ -114,9 +151,10 @@ app.layout = html.Div(children=[
             html.Div(
                 dcc.Slider(
                     id='hour-slider',
+                    included = False,
                     min=0,
                     max=23,
-                    value=0,
+                    value=0,    # start at zero hour
                     marks={str(hour): str(hour) for hour in range(0,23,2)},
                     step=None
                 ),
@@ -137,13 +175,13 @@ app.layout = html.Div(children=[
             dcc.Graph(
                 id='temp-out',
             ),
-            style={'width':'40%','display':'inline-block'},
+            style={'width':'50%','display':'inline-block'},
         ),
         html.Span(
             dcc.Graph(
                 id='rel-hum',
             ),
-            style={'width':'40%','display':'inline-block'},
+            style={'width':'50%','display':'inline-block'},
         ),
         ]
     ),
@@ -155,13 +193,14 @@ app.layout = html.Div(children=[
     [Input('wind-date-slider', 'value')])
 def update_wind_speed_figure(selected_day):
     f_df = df30[str(date0+dt.timedelta(days=selected_day))]
+    logger.debug('selected_day=%d, date=%s, rows=%d' %(selected_day,str(date0+dt.timedelta(days=selected_day)),f_df.shape[0]))
     traces = []
     traces.append(dict(x=f_df.index,y=f_df['wind_speed'],type='line',name=u'wind_speed'))
     traces.append(dict(x=f_df.index,y=f_df['wind_gust'],type='line',name=u'wind_gust'))
 
     return {
         'data': traces,
-        'layout': dict(title={'text':'Wind Speed (every 5 minutes)','y':'0.8'}),
+        'layout': dict(height=400,title={'text':'Wind Speed (every 5 minutes)','y':'0.8'}),
     }
 
 @app.callback(
@@ -173,7 +212,7 @@ def update_temp_out_figure(selected_day):
     traces.append(dict(x=f_df.index,y=f_df['temp_out'],type='line',name=u'temperature'))
     return {
         'data': traces,
-        'layout': dict(title={'text':'Temperature Outside','y':'0.8'}, height=400),
+        'layout': dict(title={'text':'Temperature Outside','y':'0.8'}, height=300),
     }
 
 @app.callback(
@@ -186,7 +225,7 @@ def update_rel_hum_figure(selected_day):
 
     return {
         'data': traces,
-        'layout': dict(title={'text':'Relative Humidity','y':'0.8'}, height=400),
+        'layout': dict(title={'text':'Relative Humidity','y':'0.8'}, height=300),
     }
 
 
@@ -222,7 +261,7 @@ def update_wind_dir_figure(selected_day, selected_hour):
     ]
 
     layout = dict(
-        height=350,
+        height=350, width=400,
         #plot_bgcolor=app_color["graph_bg"],
         paper_bgcolor=app_color["graph_bg"],
         font={"color": "#fff"},
